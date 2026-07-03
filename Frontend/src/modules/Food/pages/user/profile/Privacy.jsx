6bd5ffbb6603b7dffedf1aa534bf74edec66ca02
payalsisodiya55 <payalsisodiya038@gmail.com>
2026-07-03 12:25:36 +0530

    fixed name

--- Frontend/src/modules/Food/pages/user/profile/Privacy.jsx
+++ Frontend/src/modules/Food/pages/user/profile/Privacy.jsx
@@ -115,7 +115,7 @@ export default function Privacy() {
 
         <p className="text-center mt-10 text-[10px] text-gray-400 font-black uppercase tracking-[0.2em] leading-relaxed">
           Last updated: {new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })} <br />
-          © {new Date().getFullYear()} Foodelo. All Rights Reserved.
+          © {new Date().getFullYear()} Tiffinji. All Rights Reserved.
         </p>
       </div>
     </AnimatedPage>
